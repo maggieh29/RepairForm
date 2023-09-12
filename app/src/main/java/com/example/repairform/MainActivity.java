9f1a7fc083306fc04b8024c3b8be52763ca47468
Maggie Harvey <mharvey82902@gamil.com>
2023-09-12 12:38:29 -0500

    Initial commit Creating boilerplate project

---
+++ app/src/main/java/com/example/repairform/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.repairform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttSubmit;
+    EditText inputName;
+    String nameInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //assigning the widget created in the xml to a variable in java code
+        buttSubmit = findViewById(R.id.buttSubmit);
+        inputName = findViewById(R.id.inputName);
+
+        
+        nameInput = inputName.getText().toString();
+
+
+
+    }
+}
