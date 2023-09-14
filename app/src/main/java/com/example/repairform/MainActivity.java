e0024e1e960b2a70571a555f62b8eeb8f661ef05
Maggie Harvey <mharvey82902@gamil.com>
2023-09-14 13:21:38 -0500

    Added LOTS of references, assigned button to an event listener, assigns edit text inputs to string variables

--- app/src/main/java/com/example/repairform/MainActivity.java
+++ app/src/main/java/com/example/repairform/MainActivity.java
@@ -3,24 +3,87 @@ package com.example.repairform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttSubmit;
+    View.OnClickListener submitListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            getInputs();
 
+            double subtotal = 20;
+            subtotalNum.setText("$ " + String.valueOf(subtotal));
+
+        }
+    };
+
+
+    //Need to create a variable in code that is the same type of widget in the xml
+    Button buttSubmit;
+    //text view
+    TextView subtotalNum;
+    TextView totalNum;
+    TextView taxNum;
+    //edit text
+    EditText orderInput;
+    EditText techInput;
+    EditText inspectionInput;
+    EditText paintInput;
+    EditText partsInput;
+    EditText laborInput;
+    String order;
+    String technician;
+    String inspection;
+    String paint;
+    String parts;
+    String labor;
 
     @Override
+    //the first method to be called by default, like the main in java
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //load the xml activity and initialize it
         setContentView(R.layout.activity_main);
 
         //assigning the widget created in the xml to a variable in java code
         buttSubmit = findViewById(R.id.buttSubmit);
+        subtotalNum = findViewById(R.id.subtotalNum);
+        totalNum = findViewById(R.id.totalNum);
+        taxNum = findViewById(R.id.taxNum);
+        //all text inputs
+        orderInput = findViewById(R.id.orderInput);
+        techInput = findViewById(R.id.technicianInput);
+        inspectionInput = findViewById(R.id.inspectionInput);
+        paintInput = findViewById(R.id.paintInput);
+        partsInput = findViewById(R.id.partsInput);
+        laborInput = findViewById(R.id.laborInput);
 
+
+
+
+        /**now you can manipulate the actions of the variables
+        buttSubmit.setText("Hello"); **/
+
+        //Assign button to listener
+        buttSubmit.setOnClickListener(submitListener);
 
 
 
     }
+    //takes all text fields and assigns them to their string variables
+    public void getInputs(){
+        order = orderInput.getText().toString();
+        technician = techInput.getText().toString();
+        inspection = inspectionInput.getText().toString();
+        paint = paintInput.getText().toString();
+        parts = partsInput.getText().toString();
+        labor = laborInput.getText().toString();
+    }
+
 }
